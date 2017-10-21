02af77dc8520e6be5f1ef7beba62ce51160baa1b
nataliefriese <friese@mail.upb.de>
2017-10-21 21:08:36 +0200

    initial commit of solution to 2.2

---
+++ MachineLearning2/Abalone.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MachineLearning2
+{
+    class Abalone
+    {
+        public double[] Values = new double[8];
+        public int Rings;
+        public int Prediction;
+
+       public Abalone(String input)
+        {
+            String[] help = input.Split(' ');
+            for (int i = 0; i < Values.Length; i++)
+            {
+                Values[i] = Convert.ToDouble(help[i]);
+            }
+            
+            Rings = Convert.ToInt32(help[8].Split(',')[0]);
+        }
+
+        public Abalone GetNearest(List<Abalone> data, List<Abalone> nearest)
+        {
+            double smallestDistance = double.PositiveInfinity;
+            Abalone Nearest = null;
+            foreach (var aba in data)
+            {
+                bool found = false;
+
+                foreach (var aba2 in nearest)
+                {
+                    if (aba.Equals(aba2))
+                    {
+                        found = true;
+                    }
+                }
+                if (aba.Equals(this))
+                {
+                    found = true;
+                }
+                if (!found)
+                {
+                    double distance = 0;
+                    if (Values[0] != aba.Values[0])
+                    {
+                        distance += 1;
+                    }
+                    for (int i = 1; i < Values.Length; i++)
+                    {
+                        distance += (Values[i] - aba.Values[i]) * (Values[i] - aba.Values[i]) * 100;
+                    }
+
+
+                    if (Math.Sqrt(distance) < smallestDistance)
+                    {
+                        smallestDistance = Math.Sqrt(distance);
+                        Nearest = aba;
+                    }
+                }
+            }
+
+            return Nearest;
+        }
+
+        public void SetPrediction(List<Abalone> nearest)
+        {
+            int pred = 0;
+            foreach (var aba in nearest)
+            {
+                pred += aba.Rings;
+            }
+            Prediction= (int) Math.Round((double)(pred / nearest.Count), 0);
+        }
+    }
+}
--- MachineLearning2/Program.cs
+++ MachineLearning2/Program.cs
@@ -10,6 +10,53 @@ namespace MachineLearning2
     {
         static void Main(string[] args)
         {
+            string[] lines =
+                System.IO.File.ReadAllLines(
+                    @"C:\Users\Natalie\source\repos\MachineLearning2\MachineLearning2\abalone.txt");
+
+            List<Abalone> schneckschens = new List<Abalone>();
+            foreach (var line in lines)
+            {
+                schneckschens.Add(new Abalone(line));
+            }
+
+            for (int k = 1; k <= 10; k++)
+            {
+                
+                foreach (var aba in schneckschens)
+                {
+                    List<Abalone> nearest = new List<Abalone>();
+                    for (int i = 0; i < k; i++)
+                    {
+                        nearest.Add(aba.GetNearest(schneckschens, nearest));
+                    }
+                    aba.SetPrediction(nearest);
+                }
+
+
+                //get Errors
+                int absoluteErrors = 0;
+
+                foreach (var gary in schneckschens)
+                {
+                    if (gary.Prediction < gary.Rings)
+                    {
+                        absoluteErrors += gary.Rings - gary.Prediction;
+                    }
+                    else
+                    {
+                        absoluteErrors += gary.Prediction - gary.Rings;
+                    }
+                }
+                double avg = absoluteErrors*1.0 / schneckschens.Count;
+                Console.WriteLine("k = " + k + " Error rate: " + avg);
+                
+            }
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
         }
+
+
+
     }
 }
